f99edb2f6f6314d2cb0f3818e3cc74e98767e2ab
masch <the.masch@gmail.com>
2025-12-17 20:28:07 -0300

    feat: Add `onSubmit` handler to search form

--- 02-react/src/components/SearchFormSection.jsx
+++ 02-react/src/components/SearchFormSection.jsx
@@ -71,7 +71,7 @@ export function SearchFormSection({ initialText, onSearch, onTextFilter }) {
             <h1>Encuentra tu próximo trabajo</h1>
             <p>Explora miles de oportunidades en el sector tecnológico.</p>
 
-            <form onChange={handleSubmit} id="empleos-search-form" role="search">
+            <form onChange={handleSubmit} onSubmit={handleSubmit} id="empleos-search-form" role="search" >
                 <div className="search-bar">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                         stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"
