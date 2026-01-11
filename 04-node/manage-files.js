c70d2cf211b412d8944e09e5ad0da21ec293ae95
masch <the.masch@gmail.com>
2026-01-11 12:58:33 -0300

    feat: validate permission

--- 04-node/manage-files.js
+++ 04-node/manage-files.js
@@ -1,17 +1,26 @@
 import { mkdir, readFile, writeFile } from "node:fs/promises";
 import { join, basename, extname } from "node:path";
 
-const content = await readFile("file.txt", "utf-8");
-console.log(content);
+let content = ''
+if (process.permission.has('fs.read', 'archivo.txt')) {
+    content = await readFile("file.txt", "utf-8");
+    console.log(content);
+} else {
+    console.log("No tienes permiso para leer el archivo");
+}
 
-const output = join("output", "files");
-await mkdir(output, { recursive: true });
+if (process.permission.has('fs.write', 'archivo.txt')) {
+    const output = join("output", "files");
+    await mkdir(output, { recursive: true });
 
-const upperContent = content.toUpperCase();
-const outputFilePath = join(output, "file-uppercased.txt");
+    const upperContent = content.toUpperCase();
+    const outputFilePath = join(output, "file-uppercased.txt");
 
-console.log(`The file extension of "${outputFilePath}" is "${extname(outputFilePath)}"`);
-console.log(`The file name of "${outputFilePath}" is "${basename(outputFilePath)}"`);
+    console.log(`The file extension of "${outputFilePath}" is "${extname(outputFilePath)}"`);
+    console.log(`The file name of "${outputFilePath}" is "${basename(outputFilePath)}"`);
 
-await writeFile(outputFilePath, upperContent);
-console.log("File uppercased");
+    await writeFile(outputFilePath, upperContent);
+    console.log("File uppercased");
+} else {
+    console.log("No tienes permiso para escribir el archivo");
+}
