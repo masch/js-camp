c06a47a428ffa67f696acb5d4518d58a2beda754
masch <the.masch@gmail.com>
2026-01-12 22:23:02 -0300

    fix: Manage files location

--- 04-node/manage-files.js
+++ 04-node/manage-files.js
@@ -2,14 +2,14 @@ import { mkdir, readFile, writeFile } from "node:fs/promises";
 import { join, basename, extname } from "node:path";
 
 let content = ''
-if (process.permission.has('fs.read', 'archivo.txt')) {
+if (process.permission.has('fs.read', 'file.txt')) {
     content = await readFile("file.txt", "utf-8");
     console.log(content);
 } else {
     console.log("No tienes permiso para leer el archivo");
 }
 
-if (process.permission.has('fs.write', 'archivo.txt')) {
+if (process.permission.has('fs.write', 'output/files/file-uppercased.txt')) {
     const output = join("output", "files");
     await mkdir(output, { recursive: true });
 
