ea43b1742d3c092290bf5508da6f76a25a2ea641
masch <the.masch@gmail.com>
2026-01-18 15:07:09 -0300

    feat(jobs): add job listing and retrieval endpoints
    Introduce job data and endpoints to the Express application.
    
    Changes:
    - Add jobs.json with a collection of job listings.
    - Add /get-jobs endpoint to serve the complete list of jobs.
    - Add /get-job/:id endpoint with initial implementation for
      retrieving job details by ID.
    - Update index.js to support JSON file imports in ESM.

--- 04-express/index.js
+++ 04-express/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import jobs from "./jobs.json" with { type: "json" }; // In order to read JSON files on ESM
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -6,6 +7,33 @@ const app = express();
 app.get("/", (req, res) => {
     return res.json({
         message: "Hello World!",
+    });
+});
+
+app.get("/get-jobs", (req, res) => {
+
+    return res.json({
+        jobs
+    });
+});
+
+app.get("/get-job/:id", (req, res) => {
+    const { id } = req.params;
+
+    const idNumber = Number(id);
+
+    if (isNaN(idNumber)) {
+        return res.status(400).json({
+            message: "Invalid job ID",
+        });
+    }
+
+    return res.json({
+        job: {
+            id: idNumber,
+            title: "Job 1",
+            description: "Description 1",
+        },
     });
 });
 
