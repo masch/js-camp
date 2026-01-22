9f37cce77fc730a82541cb4806ce0682b12337ae
masch <the.masch@gmail.com>
2026-01-21 22:36:29 -0300

    chore: Log cors endpoint

--- 04-express/middlewares/cors.js
+++ 04-express/middlewares/cors.js
@@ -8,6 +8,7 @@ const ACCEPTED_ORIGINS = [
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => {
     return cors({
         origin: (origin, callback) => {
+            console.log({ origin });
             if (acceptedOrigins.includes(origin)) {
                 return callback(null, true);
             }
