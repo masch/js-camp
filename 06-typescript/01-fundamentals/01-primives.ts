ae6d2be52be748dd87e88ac0d31e104269ce2c47
masch <the.masch@gmail.com>
2026-02-09 21:40:31 -0300

    feat(06-ts) First steps

---
+++ 06-typescript/01-fundamentals/01-primives.ts
@@ -0,0 +1,25 @@
+//1. Strings
+
+const nombre: string = 'pepe'
+const saludo = `Hola ${nombre}`
+const vacio: string = ""
+
+//2. numeros
+let color = 0x09f
+let infinity = Infinity
+
+//3. Booleans
+let isActive: boolean = true
+isActive = false
+
+// 4. null and undefined
+let nulo: null = null
+let undefinedVar: undefined = undefined
+
+
+let age: number | null = null
+age = 30
+
+const bigNumber: bigint = 2332323n
+const id: symbol = Symbol("id")
+
---
+++ 06-typescript/01-fundamentals/02-arrays.ts
@@ -0,0 +1,19 @@
+
+// Sintaxis 1
+const numbers: number[] = [1, 2, 3, 4, 5];
+numbers.push(6)
+
+// Tuple
+const fruits: [string, string, string] = ["apple", "banana", "orange"]
+
+// Sintaxis 2
+const numbers2: Array<number> = [1, 4, 4, 4,]
+numbers2.push(6)
+
+const empty = []
+
+// Mixto array
+const mixto: (number | null)[] = [1, null]
+
+
+
